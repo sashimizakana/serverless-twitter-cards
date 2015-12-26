85487d7cc8b62c07d6590aa62c51a6558fef861e
sashimizakana <atto_999@hotmail.com>
2015-12-27 04:21:56 +0900

    対象URLの検証追加

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@
 var client = require('cheerio-httpcli');
 var config = require('./config.js');
 exports.handler = function(evt,context){
-    client.fetch(decodeURIComponent(evt.url)).then(function(result){
+    var url = decodeURIComponent(evt.url);
+    if(!config.URL_MATCH.test(url)){
+        context.fail('Bad Request: invalid URL');
+        return;
+    }
+    client.fetch(url).then(function(result){
         var image = result.$(config.IMAGE_SELECTER);
         context.succeed({
             location:image.length > 0 ? image.eq(0).attr('src') : config.DEFAULT_IMAGE_URL
